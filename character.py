ea16e33d3acc21b58b59af430d6432b2fee002ee
oh taeseok <kbbong03@tukorea.ac.kr>
2025-10-18 01:50:29 +0900

    캐릭터 기본 상태 함수 추가 및 움직임 클래스 추가

--- character.py
+++ character.py
@@ -2,7 +2,17 @@ from pico2d import load_image, get_time
 from worldmap import *
 from sdl2 import *
 
-
+class move:
+    def __init__(self, character):
+        self.character = character
+    def enter(self, e):
+        pass
+    def exit(self):
+        pass
+    def do(self):
+        pass
+    def draw(self, e):
+        pass
 
 class character:
     def __init__(self):
@@ -10,7 +20,9 @@ class character:
         self.x = WorldMap.width/2
         self.y = WorldMap.height/2
         self.frame = 0
+        self.updown_dir = 1
         self.face_dir = 1
+        self.dir = 0
         self.max_dash = 2
         self.can_dash = self.max_dash
         self.weapon_type = katana # 카타나 또는 대검
