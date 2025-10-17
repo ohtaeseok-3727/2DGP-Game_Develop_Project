1ebc26ae4696525096ed271b28f133fa4ab6a38d
oh taeseok <kbbong03@tukorea.ac.kr>
2025-10-18 01:55:21 +0900

    메인 파일 생성

---
+++ main.py
@@ -0,0 +1,13 @@
+from pico2d import *
+from character import character
+from worldmap import WorldMap
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
