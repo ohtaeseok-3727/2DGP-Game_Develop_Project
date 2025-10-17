ab16f2c9015b0919285328d36d8514e660427805
oh taeseok <kbbong03@tukorea.ac.kr>
2025-10-18 01:38:38 +0900

    맵 함수 기본 설정

--- worldmap.py
+++ worldmap.py
@@ -1,4 +1,10 @@
 from pico2d import *
 
 class WorldMap:
-    def __init__(self):
+    def __init__(self, width = 1366, height = 768, cell = 64):
+        self.width = width
+        self.height = height
+        self.cell = cell
+        self.cols = self.width // self.cell
+        self.rows = self.height // self.cell
+    def
