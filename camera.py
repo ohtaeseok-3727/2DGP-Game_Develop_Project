23d2a3ab9db32c3c6d5857e6f8e27cd8e7b4ad84
oh taeseok <kbbong03@tukorea.ac.kr>
2025-10-26 17:02:46 +0900

    카메라 클래스 추가

---
+++ camera.py
@@ -0,0 +1,11 @@
+from pico2d import *
+
+class Camera:
+    def __init__(self, target):
+        pass
+    def update(self):
+        pass
+    def set_for_draw(self):
+        pass
+    def unset_for_draw(self):
+        pass
