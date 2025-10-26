ba78aa38fef99115112b962b966c6e4923d4bff6
oh taeseok <kbbong03@tukorea.ac.kr>
2025-10-26 17:03:47 +0900

    기본 변수 추가 및 카메라의 위치를 타겟(플레이어 캐릭터)로 옮기기

--- camera.py
+++ camera.py
@@ -2,8 +2,15 @@ from pico2d import *
 
 class Camera:
     def __init__(self, target):
+        self.target = target
+        self.x = 0
+        self.y = 0
+        self.screen_width = get_canvas_width()
+        self.screen_height = get_canvas_height()
         pass
     def update(self):
+        self.x = self.target.x - self.screen_width // 2
+        self.y = self.target.y - self.screen_height // 2
         pass
     def set_for_draw(self):
         pass
