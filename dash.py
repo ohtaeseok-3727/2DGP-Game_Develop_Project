c93993f7a523d8cbb18ce81927110e5461393ca1
oh taeseok <kbbong03@tukorea.ac.kr>
2025-10-28 13:43:06 +0900

    클래스 재정의

--- dash.py
+++ dash.py
@@ -6,4 +6,13 @@ from pico2d import get_time
 class dashstate :
     def __init__(self):
         pass
-    
+    def can_dash(self):
+        pass
+    def start(self):
+        pass
+    def update(self):
+        pass
+    def stop(self):
+        pass
+    def on_input(self, event):
+        pass
