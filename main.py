e78eb60a790f9730735efa0cc930c489a4510061
oh taeseok <kbbong03@tukorea.ac.kr>
2025-11-10 14:21:19 +0900

    게임화면에서 기본 마우스 포인터 숨기기

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pico2d import *
+from sdl2 import SDL_ShowCursor, SDL_DISABLE
 import title_mode as start_mode
 import game_framework
 
 open_canvas(1366, 768)
+
+SDL_ShowCursor(SDL_DISABLE)
+
 game_framework.run(start_mode)
 close_canvas()
