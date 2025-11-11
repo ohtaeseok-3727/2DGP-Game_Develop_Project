9283e006d008b0f6b6497e61bb2c93f1c7ee6444
oh taeseok <kbbong03@tukorea.ac.kr>
2025-11-11 23:21:29 +0900

    존재하는 않는 오브젝트를 지우는 오류 수정

--- game_world.py
+++ game_world.py
@@ -23,13 +23,16 @@ def remove_collision_object(o):
         if o in pair[1]:
             pair[1].remove(o)
 
+
 def remove_object(o):
     for layer in world:
         if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
             return
 
-    raise Exception("World 에 존재하지 않는 오브젝트를 지우려고 시도함")
+    # 오류 대신 경고 출력
+    print(f"경고: 이미 제거된 오브젝트 {o}")
 
 
 def update():
