4cbc8e79f9e93632814d4e8bb136896b253e1e4a
oh taeseok <kbbong03@tukorea.ac.kr>
2025-11-15 20:48:47 +0900

    아이템 스프라이트 및 모듈 추가

---
+++ item.py
@@ -0,0 +1,17 @@
+from pico2d import *
+import math
+import game_framework
+import game_world
+from state_machine import StateMachine
+
+class Item:
+    items = []
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, camera=None):
+        pass
+
