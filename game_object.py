9f07c7e8f9d2eafdda378c674990e4fd38f5af98
oh taeseok <kbbong03@tukorea.ac.kr>
2025-11-15 21:48:21 +0900

    오브젝트를 스프라이트로 그리도록 수정

--- game_object.py
+++ game_object.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import math
 from worldmap import WorldMap
-
+import game_framework
 
 class anvil:
     def __init__(self):
@@ -9,7 +9,15 @@ class anvil:
         self.x = 300
         self.y = 200
         self.interaction_range = 30
+
+        self.frame_width = 12
+        self.frame_height = 15
+        self.total_frames = 11
+        self.current_frame = 0
+        self.frame_time = 0
+        self.fps = 10
     def update(self, camera=None):
+
         pass
     def in_range(self, character):
         distance = math.sqrt((self.x - character.x) ** 2 + (self.y - character.y) ** 2)
@@ -33,6 +41,11 @@ class Sephrite:
         self.interaction_range = 30
         pass
     def update(self, camera=None):
+        self.frame_time += game_framework.frame_time
+
+        if self.frame_time > 1.0 / self.fps:
+            self.current_frame = (self.current_frame + 1) % self.total_frames
+            self.frame_time = 0
         pass
     def in_range(self, character):
         distance = math.sqrt((self.x - character.x) ** 2 + (self.y - character.y) ** 2)
@@ -41,10 +54,19 @@ class Sephrite:
 
     def draw(self, camera=None):
         if camera:
-            screen_x, screen_y = camera.apply(self.x, self.y)
+            sx, sy = camera.apply(self.x, self.y)
             zoom = camera.zoom
-            self.image.draw(screen_x, screen_y,
-                          self.image.w * zoom,
-                          self.image.h * zoom)
         else:
-            self.image.draw(self.x, self.y)
+            sx, sy = self.x, self.y
+            zoom = 1.0
+
+        frame_x = int(self.current_frame) * self.frame_width
+
+        Sephrite.image.clip_draw(
+            frame_x, 0,
+            self.frame_width, self.frame_height,
+            sx, sy,
+            self.frame_width * zoom, self.frame_height * zoom
+        )
+    def get_bb(self):
+        return self.x - 6, self.y - 7, self.x + 6, self.y + 7
