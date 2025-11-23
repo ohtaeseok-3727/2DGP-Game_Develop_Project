22c90993b38817e7db60cce269562e75af587613
oh taeseok <kbbong03@tukorea.ac.kr>
2025-11-23 23:14:31 +0900

    각 모드마다 다른 배경을 삽입하도록 변경

--- worldmap.py
+++ worldmap.py
@@ -1,23 +1,45 @@
-from pico2d import *
+from pico2d import load_image
 
 class WorldMap:
+    _instance = None
+    maps = {
+        'default': 'resource/map/village_background.png',
+    }
     width = 800
     height = 600
-    cell = 64
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(WorldMap, cls).__new__(cls)
+        return cls._instance
 
     def __init__(self):
-        self.image = load_image("resource/map/village_background.png")
-        self.cols = self.width // self.cell
-        self.rows = self.height // self.cell
-    def update(self, camera=None):
+        if not hasattr(self, 'initialized'):
+            self.image = None
+            self.width = 0
+            self.height = 0
+            self.change_map('default')
+            self.initialized = True
+
+    def change_map(self, map_name):
+        map_path = self.maps.get(map_name)
+        if map_path:
+            self.image = load_image(map_path)
+            self.width = self.image.w
+            self.height = self.image.h
+        else:
+            print(f"'{map_name}'에 해당하는 맵을 찾을 수 없습니다.")
+
+    def draw(self, camera=None):
+        if camera:
+            screen_x, screen_y = camera.apply(0, 0)
+            zoom = camera.zoom
+            self.image.draw(screen_x + (self.width * zoom) / 2,
+                            screen_y + (self.height * zoom) / 2,
+                            self.width * zoom,
+                            self.height * zoom)
+        else:
+            self.image.draw(self.width // 2, self.height // 2)
+
+    def update(self):
         pass
-    def draw(self, camera):
-        for x in range(0, WorldMap.width + 1, WorldMap.cell):
-            sx1, sy1 = camera.apply(x, 0)
-            sx2, sy2 = camera.apply(x, WorldMap.height)
-            draw_line(sx1, sy1, sx2, sy2)
-        for y in range(0, WorldMap.height + 1, WorldMap.cell):
-            sx1, sy1 = camera.apply(0, y)
-            sx2, sy2 = camera.apply(WorldMap.width, y)
-            draw_line(sx1, sy1, sx2, sy2)
-        self.image.draw(WorldMap.width // 2, WorldMap.height // 2)
